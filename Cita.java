88d2c88cc3053cf46ed3ce3658f901ce2bdda787
unknown <nicoaldopaez@gmail.com>
2024-04-17 01:20:17 +0200

    correccion3

--- Cita.java
+++ Cita.java
@@ -8,19 +8,21 @@ class Cita {
 
     public Cita(String paciente, String fecha, String hora) {
         this.paciente = paciente;
-        this.fecha = LocalDate.parse(fecha, DateTimeFormatter.ofPattern("d/M/yyyy"));
-        this.hora = LocalTime.parse(hora);
+        this.fecha = LocalDate.parse(fecha, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        this.hora = LocalTime.parse(hora, DateTimeFormatter.ofPattern("HH:mm"));
     }
 
     public String getPaciente() {
         return paciente;
     }
 
-    public LocalDate getFecha() {
-        return fecha;
+    public String getFecha() {
+        DateTimeFormatter fechaFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return fecha.format(fechaFormat);
     }
 
-    public LocalTime getHora() {
-        return hora;
+    public String getHora() {
+        DateTimeFormatter horaFormat = DateTimeFormatter.ofPattern("HH:mm");
+        return hora.format(horaFormat);
     }
 }
