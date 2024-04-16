a10c32e93120625c4c9d61f9c0f963bd75fefd7c
Nicolas <nicoaldopaez@gmail.com>
2024-04-16 13:12:47 +0200

    Ampliando el proyecto clinica-massalud corregido

--- Cita.java
+++ Cita.java
@@ -1,14 +1,15 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 class Cita {
     private String paciente;
     private LocalDate fecha;
     private LocalTime hora;
 
-    public Cita(String paciente, String fecha, LocalTime hora) {
+    public Cita(String paciente, String fecha, String hora) {
         this.paciente = paciente;
-        this.fecha = LocalDate.parse(fecha);
-        this.hora = hora;
+        this.fecha = LocalDate.parse(fecha, DateTimeFormatter.ofPattern("d/M/yyyy"));
+        this.hora = LocalTime.parse(hora);
     }
 
     public String getPaciente() {
